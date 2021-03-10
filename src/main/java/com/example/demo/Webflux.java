c50761bd21f611811034d0d1e3134c926517cc22
Omar Ayoub <omar@abstract.ma>
2021-03-10 09:13:18 +0100

    Init

---
+++ src/main/java/com/example/demo/Webflux.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class Webflux {
+
+    @GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "Flux - " + LocalTime.now().toString());
+    }
+}
